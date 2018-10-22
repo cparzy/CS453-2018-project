d5e4f52dc86937e1d0d4522ef64d68f564aca2a6
Igor Zablotchi <igor.zablotchi@epfl.ch>
2018-10-22 18:07:19 +0200

    calling python script from makefile

--- grading/run.py
+++ grading/run.py
@@ -5,16 +5,17 @@ import re
 
 args = sys.argv
 
-# usage: run.py reference target numrepetitions
+# usage: run.py grading_binary reference target numrepetitions
 
-reference = sys.argv[1]
-target = sys.argv[2]
-numrepetitions = int(sys.argv[3])
+binary = sys.argv[1]
+reference = sys.argv[2]
+target = sys.argv[3]
+numrepetitions = int(sys.argv[4])
 
 # reference
 times = []
 for rep in range(numrepetitions):
-    output = subprocess.check_output("./grading -t " + reference, shell=True).decode("utf-8")
+    output = subprocess.check_output(binary + " -t " + reference, shell=True).decode("utf-8")
     print(output)
     m = re.search("\((\d+)", output)
     times.append(int(m.group(1)))
@@ -25,7 +26,7 @@ print("Reference time: ", ref_time)
 # target
 times = []
 for rep in range(numrepetitions):
-    output = subprocess.check_output("./grading -t " + target, shell=True).decode("utf-8")
+    output = subprocess.check_output(binary + " -t " + target, shell=True).decode("utf-8")
     print(output)
     m = re.search("\((\d+)", output)
     times.append(int(m.group(1)))
